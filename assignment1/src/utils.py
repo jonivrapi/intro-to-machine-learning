be867536cd252478f1d3d3619519c523a1c5a3d2
Joni Vrapi <jonivrapi@jonis-mbp.lan>
2023-02-03 20:39:16 -0500

    WIP

--- assignment1/src/utils.py
+++ assignment1/src/utils.py
@@ -1,1 +1,20 @@
-print('hello world')
+import pandas as pd
+class MLPipelineSetup:
+    def __init__(self) -> None:
+        self.datasetPaths = [
+            "../../datasets/abalone/abalone.data",
+            "../../datasets/breast-cancer/breast-cancer.data",
+            "../../datasets/car/car.data",
+            "../../datasets/forest-fires/forest-fires.data",
+            "../../datasets/house-votes/house-votes.data",
+            "../../datasets/machine/machine.data"
+        ]
+    
+    def loadData(path):
+        return pd.read_table(path, sep=',', header=None)
+    
+    # def handleMissingValues():
+
+
+for path in datasetPaths:
+    print(loadData(path))
